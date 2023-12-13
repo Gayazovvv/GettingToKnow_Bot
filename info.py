0614eb1079807683b80a40e3cabff995f48450a5
unknown <bulatgaazov123@gmail.com>
2023-12-13 19:44:17 +0300

    bot visit

---
+++ info.py
@@ -0,0 +1,13 @@
+character_description = {"name": "Булат",
+                         "age": "16",
+                         "city": "Казань",
+                         "study": "Школа, 9 класс"}
+
+contacts = {"email" : "bulatgaazov123@gmail.com",
+            "number" : "8-800-555-35-35",
+            "Telegram" : "@im_cupper",
+            "VK" : "@facepulm",
+            "GIT" : "@Gayazovvv"}
+
+
+
